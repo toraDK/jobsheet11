d77989d9eb37d0c7c7abe6e6cbd740c4ea8a8465
tora180405 <toradigda@gmail.com>
2023-11-09 13:53:05 +0700

    tambah file

---
+++ BintangPersegi27.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BintangPersegi27 {
+
+    public static void main(String[] args) {
+        Scanner sc27 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = sc27.nextInt();
+        
+        for(int iouter = 1; iouter <= N; iouter++){
+            for(int i=1; i<=N; i++){
+                System.out.print("*");
+            }
+        }
+    }
+}
